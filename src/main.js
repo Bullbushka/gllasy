ef4a587723b3c6b2a81a87b0311f371918e684dd
Vanda Konovalova <konovalovavanda@gmail.com>
2021-07-24 11:05:32 +0300

    add dropdown window

--- src/main.js
+++ src/main.js
@@ -1,9 +1,3 @@
-const feedback = document.querySelector(".contacts__button");
-const backdrop = document.querySelector(".backdrop");
-const modalFeedback = document.querySelector(".feedback");
-const close = document.querySelector(".button-close");
-const fio = modalFeedback.querySelector("[name=FIO]");
-
 const slider = document.querySelector(".slider");
 const slide1 = slider.querySelector("[value='1']");
 const slide2 = slider.querySelector("[value='2']");
@@ -41,6 +35,47 @@ slide1.addEventListener("click", function(evt) {
     promo3.classList.remove("js_promo_show");
 });
 
+
+const login = document.querySelector(".login");
+const modalLogin = login.querySelector(".login__form");
+const buttonLogin = login.querySelector(".login__button");
+const search = document.querySelector(".search");
+const modalSearch = search.querySelector(".search .search__form");
+const buttonSearch = search.querySelector(".search .search__button");
+
+function dropdown(clickedElement, elementToShow, legalElement) {
+    clickedElement.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        elementToShow.classList.toggle("js_show");
+        clickedElement.classList.toggle("js_hover");
+    });
+
+    document.body.addEventListener("click", function(evt) {
+        if (!legalElement.contains(evt.target)) {
+            elementToShow.classList.remove("js_show");
+            clickedElement.classList.remove("js_hover");
+        }
+    })
+}
+
+dropdown(buttonLogin, modalLogin, login);
+dropdown(buttonSearch, modalSearch, search);
+
+
+const feedback = document.querySelector(".contacts__button");
+const backdrop = document.querySelector(".backdrop");
+const modalFeedback = document.querySelector(".feedback");
+const closeButton = document.querySelector(".button-close");
+const fio = modalFeedback.querySelector("[name=FIO]");
+
+function closeModal(closeElement) {
+    closeElement.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        backdrop.classList.remove("modal-show");
+        modalFeedback.classList.remove("modal-show");
+    });
+}
+
 feedback.addEventListener("click", function(evt) {
     evt.preventDefault();
     backdrop.classList.add("modal-show");
@@ -48,17 +83,8 @@ feedback.addEventListener("click", function(evt) {
     fio.focus();
 });
 
-close.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    backdrop.classList.remove("modal-show");
-    modalFeedback.classList.remove("modal-show");
-});
-
-backdrop.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    backdrop.classList.remove("modal-show");
-    modalFeedback.classList.remove("modal-show");
-});
+closeModal(closeButton);
+closeModal(backdrop);
 
 window.addEventListener("keyup", function(evt) {
     if (evt.code === "Escape") {
